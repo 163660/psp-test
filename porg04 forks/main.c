d82c5c9578086b6584a94a4d324db772ab9e77a6
163660 <91126223+163660@users.noreply.github.com>
2021-10-25 10:39:38 +0200

    a

---
+++ porg04 forks/main.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+int main (int argc, char const *argv[])
+{
+    int a = 3
+    pid_t pid = fork();
+    printf("mi PID es : %d\n\tVariale a: %d" , getpid())
+    
+    return 0;
+}
