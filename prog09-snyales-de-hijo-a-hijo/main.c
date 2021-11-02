0b6aebe5c0ace1965aefebaf1cdd4ba718864aa9
163660 <91126223+163660@users.noreply.github.com>
2021-11-02 10:54:22 +0100

    a

--- prog09-snyales-de-hijo-a-hijo/main.c
+++ prog09-snyales-de-hijo-a-hijo/main.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 
 void tratarSenyal(int cod_senyal);
 
@@ -14,6 +15,7 @@ int main(int argc, char const *argv[])
         exit(-1);
     }
     if (pidHijo1 ==0){//hijo 1
+        siganl(SIGUSR1, tratarSenyal);
         printf("[Hijo 1]: pid: %d\n", getpid());
         pause(); //Espero una señal
     }else{ //padre
@@ -25,10 +27,16 @@ int main(int argc, char const *argv[])
         if(pidHijo2 ==0){//hijo 2
             printf("[Hijo 2]: pid: %d\n", getpid());
             printf("[Hijo 2]:tengo un hermano con  pid: %d\n", pidHijo1);
+            sleep(2);
             kill(pidHijo1, SIGUSR1);
 
         }else{//padre
             printf("[Padre]: he creado un hijo con pid: %d\n", pidHijo2);
+            printf("Esperando a un hijo...\n");
+            printf("El hijo con el pid %d finalizo\n" wait(NULL));
+            printf("Esperando a otro hijo...\n");
+            printf("El hijo con el pid %d finalizo\n" wait(NULL));
+            
         }
         
     }
